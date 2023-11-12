774d0ede03cc1d6864b112ddb63e640ba3c160b7
SangHoon Park <tkdgns7070@gmail.com>
2023-11-12 22:11:09 +0900

    docs: 2장 코틀린 소개  - '클래스와 프로퍼티 - 프로퍼티' 까지 완료

--- kotlin-in-action-master/src/ch02/study/MainFunction.kt
+++ kotlin-in-action-master/src/ch02/study/MainFunction.kt
@@ -4,4 +4,19 @@ fun main(args: Array<String>){
     // 대입문을 식으로 사용하려는 시도
     /*var a: Int
     var b = (a = 5)*/
+
+    // unresolved reference 에러
+    /*val message = "kotlin"
+    println("Hello, $message");
+    println("안녕 $message아 ?");
+    println("안녕 ${message}아 ?");*/
+
+    // getter, setter 기본 제공
+    val person = Person("sanghoon", false);
+    println(person.name)
+    println(person.isMarried)
+    person.isMarried = true
+    println(person.isMarried)
 }
+
+class Person(val name: String, var isMarried: Boolean)
