cd691ebdc7074a7773fd73eeeae1a6d0b1237959
YuGyeong <lyg64522@naver.com>
2023-12-11 01:16:15 +0900

    코틀린 4장 정리

--- kotlin-in-action-master/src/ch04/4.3.1.2_1_ObjectEqualityEquals.kt
+++ kotlin-in-action-master/src/ch04/4.3.1.2_1_ObjectEqualityEquals.kt
@@ -1,6 +1,6 @@
 package ch04.ex3_1_2_1_ObjectEqualityEquals
 
-class Client(val name: String, val postalCode: Int)
+data class Client(val name: String, val postalCode: Int)
 
 fun main(args: Array<String>) {
     val client1 = Client("Alice", 342562)
